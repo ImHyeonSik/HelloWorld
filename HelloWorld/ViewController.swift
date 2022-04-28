7a537ab5312a3875a3b47c8b6f0a1e714b3dcccc
hyeonsik <dgt4862@gmail.com>
2022-04-28 23:45:48 +0900

    스토리보드와 연동하여 text 값 바꾸기

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lbHello: UILabel!
+    
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Day-1
     }
 
-
+    @IBAction func btnSend(_ sender: Any) {
+        lbHello.text = "Hello, " + txtName.text!
+    }
+    
 }
 
